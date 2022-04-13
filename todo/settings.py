0c0bc150d871374e659f109cff6376348fdbf4a4
OliverReyesG <ing.oliverreyesgutierrez@gmail.com>
2022-04-13 09:17:34 -0400

    Fixed SECRET_KEY vulnerability with an environment variable

--- todo/settings.py
+++ todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9zcev%w%0%msan(v+$w&=&r7m21*phir8-d=n_d83d@i%s141%'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
